530e7b16bed5ff26ec2a6e1d0e435a8c4540a29e
sayantandolui97@gmail.com <sayantandolui97@gmail.com>
2023-09-10 00:00:37 +0530

    project source button

--- src/pages/projects/ProjectCard/ProjectCard.jsx
+++ src/pages/projects/ProjectCard/ProjectCard.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
-import './projectCard.scss'
+import "./projectCard.scss";
 
-const ProjectCard = ({title,tech,feature,timestamp,projectLink}) => {
+const ProjectCard = ({ title, tech, feature, timestamp, projectLink }) => {
   return (
     <>
-      <div className='projectCard'>
+      <div className="projectCard">
         <div className="projectImg">
           <img src="" alt="" />
         </div>
@@ -14,11 +14,18 @@ const ProjectCard = ({title,tech,feature,timestamp,projectLink}) => {
           <h3>{`[ ${tech} ]`}</h3>
           <h4>{feature}</h4>
           <h6>{`[ ${timestamp} ]`}</h6>
-          <a href={projectLink}><i className="fa-solid fa-arrow-up-right-from-square"></i></a>
+          <div className="linkBtn">
+            <a href={projectLink}>
+              <button>GitHub</button>
+            </a>
+            <a href={projectLink}>
+              <button>Live</button>
+            </a>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default ProjectCard;
