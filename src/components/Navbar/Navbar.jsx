dc5a4ea5903d3998d7fc6c6c0237a76b19940728
sayantandolui97@gmail.com <sayantandolui97@gmail.com>
2023-09-09 23:35:34 +0530

    navbar responsive design update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,10 +28,6 @@ export default function Header() {
               </li>
             </ul>
       </div>
-
-      <div className="right">
-         <i className="fa-solid fa-ellipsis"></i>
-      </div>
     </div>
   )
 }
